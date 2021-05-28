e265fbdc7cd39cdeb5ee783fcf031d1f77f81ee7
leandrozacariasaleluia <zacaleandro1985@gmail.com>
2021-05-28 00:06:34 -0300

    JavaScript-28-05-2021

---
+++ exercicios/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+console.log(`A velocidade do seu ccarro é ${vel} Km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida.MULTADO!`)
+}    
+else {
+    console.log(`Continue na velocidade da pista!`)    
+}
+console.log(`Dirija Sempre usando cinto de Segurança!`)
